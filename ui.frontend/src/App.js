bb94f05d03a9e9e79e55c1202555586189c4c460
Daniel Platon <platon@adobe.com>
2020-09-15 13:36:56 +0300

    Implement the React product carousel
    Create the AEM component, create the React component

--- ui.frontend/src/App.js
+++ ui.frontend/src/App.js
@@ -1,16 +1,17 @@
-import { Page, withModel } from '@adobe/aem-react-editable-components';
+import {Page, withModel} from '@adobe/aem-react-editable-components';
 import React from 'react';
 
 // This component is the application entry point
 class App extends Page {
-  render() {
-    return (
-      <div>
-        {this.childComponents}
-        {this.childPages}
-      </div>
-    );
-  }
+    render() {
+        console.log(`Do we have components?`, this.childComponents);
+        return (
+            <div>
+                {this.childComponents}
+                {this.childPages}
+            </div>
+        );
+    }
 }
 
 export default withModel(App);
---
+++ ui.frontend/src/components/Carousel/Carousel.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {MapTo} from '@adobe/aem-react-editable-components';
+import extractModelId from '../../utils/extract-model-id';
+import {gql, useQuery} from '@apollo/client';
+import './mockData';
+import './Carousel.css';
+
+const productsQuery = gql`
+    query getProducts($skus: [String]!) {
+        products(filter: {sku: {in: $skus}}) {
+            items {
+                sku
+                name
+                thumbnail {
+                    url
+                }
+                price_range {
+                    minimum_price {
+                        final_price {
+                            currency
+                            value
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
+
+const CarouselEditConfig = {
+    emptyLabel: 'Product Carousel',
+    isEmpty: (props) => {
+        console.log(`Do we have props? `, props);
+        return !props || !props.sku || props.sku.length < 1;
+    },
+};
+
+const Carousel = (props) => {
+    console.log(`Retrieve data for these skus ${props.sku}`);
+    const {data, loading} = useQuery(productsQuery, {
+        variables: {skus: props.sku},
+    });
+
+    if (!data) {
+        return <div>Loading...</div>;
+    }
+
+    const carouselItem = (item) => (
+        <div className="carousel_item">
+            <img src={item.thumbnail.url} alt={item.name} />
+            <h2>{item.name}</h2>
+        </div>
+    );
+
+    return (
+        <div id={extractModelId(props.cqPath)} className="carousel_root">
+            {data.products.items.map((item) => carouselItem(item))}
+        </div>
+    );
+};
+
+export default MapTo('wknd-spa-react/components/commerce/carousel')(
+    Carousel,
+    CarouselEditConfig
+);
